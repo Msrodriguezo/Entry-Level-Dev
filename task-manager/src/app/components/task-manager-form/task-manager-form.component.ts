6f3046a2bc9d77fc8a2be3e4c8eaac9270e73a71
Msrodriguezo <maicolcaballero73@gmail.com>
2025-02-11 17:58:07 -0500

    Initial Structure of paths

---
+++ task-manager/src/app/components/task-manager-form/task-manager-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-manager-form',
+  imports: [],
+  templateUrl: './task-manager-form.component.html',
+  styleUrl: './task-manager-form.component.scss'
+})
+export class TaskManagerFormComponent {
+
+}
